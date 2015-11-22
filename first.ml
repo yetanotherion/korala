29c5b55459bf4653796f8b8f72016cbcb177a9e8
Ion Alberdi <nolaridebi@gmail.com>
2015-11-22 20:26:01 +0100

    first.ml: create second voice

--- first.ml
+++ first.ml
@@ -26,78 +26,145 @@ module B = struct
     let lowest_string = 1
     let second_string = lowest_string + 1
     let third_string = second_string + 1
+    let main_start = 4
+    let bis_start = 7
 
     module F = struct
       let first = create_measure
-                    [create_string_quarter third_string 4;
-                     create_string_eighth ~dot:true third_string 4;
-                     create_string_sixteenth third_string 5;
-                     create_string_eighth ~dot:true third_string 4;
-                     create_string_sixteenth second_string 7;
-                     create_string_eighth ~dot:true second_string 5;
-                     create_string_sixteenth second_string 5]
+                    [create_string_quarter third_string main_start;
+                     create_string_eighth ~dot:true third_string main_start;
+                     create_string_sixteenth third_string (main_start + 1);
+                     create_string_eighth ~dot:true third_string main_start;
+                     create_string_sixteenth second_string (main_start + 3);
+                     create_string_eighth ~dot:true second_string (main_start + 1);
+                     create_string_sixteenth second_string (main_start + 1)]
 
-      let second = create_measure [create_string_eighth ~dot:true second_string 7;
-                                   create_string_sixteenth second_string 7;
-                                   create_string_eighth ~dot:true second_string 5;
-                                   create_string_sixteenth second_string 7;
-                                   create_string_quarter third_string 4;
-                                   create_string_quarter second_string 7]
-      let third = create_measure
-                    [create_string_quarter third_string 4;
-                     create_string_eighth ~dot:true third_string 7;
-                     create_string_sixteenth third_string 5;
-                     create_string_eighth ~dot:true third_string 4;
-                     create_string_sixteenth second_string 7;
-                     create_string_eighth ~dot:true second_string 5;
-                     create_string_sixteenth lowest_string 5]
+      let first_bis = create_measure
+                        [create_string_quarter third_string bis_start;
+                         create_string_eighth ~dot:true third_string bis_start;
+                         create_string_sixteenth third_string (bis_start + 2);
+                         create_string_eighth ~dot:true third_string bis_start;
+                         create_string_sixteenth second_string (bis_start + 3);
+                         create_string_eighth ~dot:true second_string (bis_start + 2);
+                         create_string_sixteenth second_string (bis_start + 2)]
 
-      let fourth = create_measure [create_string_eighth ~dot:true second_string 5;
-                                   create_string_sixteenth second_string 7;
-                                   create_string_eighth ~dot:true third_string 4;
-                                   create_string_sixteenth third_string 5;
-                                   create_string_quarter second_string 7;
-                                   create_string_quarter second_string 5]
+      let second = create_measure [create_string_eighth ~dot:true second_string (main_start + 3);
+                                   create_string_sixteenth second_string (main_start + 3);
+                                   create_string_eighth ~dot:true second_string (main_start + 1);
+                                   create_string_sixteenth second_string (main_start + 3);
+                                   create_string_quarter third_string main_start;
+                                   create_string_quarter second_string (main_start + 3)]
+
+      let second_bis = create_measure [create_string_eighth ~dot:true second_string (bis_start + 3);
+                                       create_string_sixteenth second_string (bis_start + 3);
+                                       create_string_eighth ~dot:true second_string (bis_start + 2);
+                                       create_string_sixteenth second_string (bis_start + 3);
+                                       create_string_quarter third_string bis_start;
+                                       create_string_quarter second_string (bis_start + 3)]
+
+      let third = create_measure [create_string_quarter third_string main_start;
+                                  create_string_eighth ~dot:true third_string (main_start + 3);
+                                  create_string_sixteenth third_string (main_start + 1);
+                                  create_string_eighth ~dot:true third_string main_start;
+                                  create_string_sixteenth second_string (main_start + 3);
+                                  create_string_eighth ~dot:true second_string (main_start + 1);
+                                  create_string_sixteenth lowest_string (main_start + 1)]
+
+      let third_bis = create_measure [create_string_quarter third_string bis_start;
+                                      create_string_eighth ~dot:true third_string (bis_start + 4);
+                                      create_string_sixteenth third_string (bis_start + 2);
+                                      create_string_eighth ~dot:true third_string bis_start;
+                                      create_string_sixteenth second_string (bis_start + 3);
+                                      create_string_eighth ~dot:true second_string (bis_start + 2);
+                                      create_string_sixteenth lowest_string (bis_start + 2)]
+
+      let fourth = create_measure [create_string_eighth ~dot:true second_string (main_start + 1);
+                                   create_string_sixteenth second_string (main_start + 3);
+                                   create_string_eighth ~dot:true third_string main_start;
+                                   create_string_sixteenth third_string (main_start + 1);
+                                   create_string_quarter second_string (main_start + 3);
+                                   create_string_quarter second_string (main_start + 1)]
+
+      let fourth_bis = create_measure [create_string_eighth ~dot:true second_string (bis_start + 2);
+                                       create_string_sixteenth second_string (bis_start + 3);
+                                       create_string_eighth ~dot:true third_string bis_start;
+                                       create_string_sixteenth third_string (bis_start + 2);
+                                       create_string_quarter second_string (bis_start + 3);
+                                       create_string_quarter second_string (bis_start + 2)]
+
       let t = [first; second; third; fourth]
+      let t_bis = [first_bis; second_bis; third_bis; fourth_bis]
       end
     module S = struct
-      let first = create_measure
-                    [create_string_sixteenth third_string 4;
-                     create_string_eighth ~dot:true second_string 5;
-                     create_string_sixteenth third_string 4;
-                     create_string_eighth ~dot:true third_string 4;
-                     create_string_sixteenth third_string 4;
-                     create_string_eighth ~dot:true ~tied:(Some `Start) third_string 5;
-                     create_string_quarter ~tied:(Some `Stop) third_string 5]
+      let first = create_measure [create_string_sixteenth third_string main_start;
+                                  create_string_eighth ~dot:true second_string (main_start + 1);
+                                  create_string_sixteenth third_string main_start;
+                                  create_string_eighth ~dot:true third_string main_start;
+                                  create_string_sixteenth third_string main_start;
+                                  create_string_eighth ~dot:true ~tied:(Some `Start) third_string (main_start + 1);
+                                  create_string_quarter ~tied:(Some `Stop) third_string (main_start + 1)]
 
-      let second = create_measure [create_string_sixteenth third_string 5;
-                                   create_string_eighth ~dot:true third_string 4;
-                                   create_string_sixteenth third_string 5;
-                                   create_string_eighth ~dot:true third_string 4;
-                                   create_string_sixteenth second_string 7;
-                                   create_string_eighth ~dot:true ~tied:(Some `Start) second_string 5;
-                                   create_string_sixteenth lowest_string 5;
-                                   create_string_eighth ~dot:true second_string 5]
+      let first_bis = create_measure [create_string_sixteenth third_string bis_start;
+                                      create_string_eighth ~dot:true second_string (bis_start + 2);
+                                      create_string_sixteenth third_string bis_start;
+                                      create_string_eighth ~dot:true third_string bis_start;
+                                      create_string_sixteenth third_string bis_start;
+                                      create_string_eighth ~dot:true ~tied:(Some `Start) third_string (bis_start + 2);
+                                      create_string_quarter ~tied:(Some `Stop) third_string (bis_start + 2)]
 
-      let third = create_measure [create_string_sixteenth second_string 7;
-                                   create_string_eighth ~dot:true third_string 4;
-                                   create_string_sixteenth third_string 5;
-                                   create_string_quarter second_string 7;
-                                   create_string_quarter ~tied:(Some `Start) second_string 7;
-                                   create_string_eighth ~dot:true ~tied:(Some `Stop) second_string 7]
+      let second = create_measure [create_string_sixteenth third_string (main_start + 1);
+                                   create_string_eighth ~dot:true third_string main_start;
+                                   create_string_sixteenth third_string (main_start + 1);
+                                   create_string_eighth ~dot:true third_string main_start;
+                                   create_string_sixteenth second_string (main_start + 3);
+                                   create_string_eighth ~dot:true ~tied:(Some `Start) second_string (main_start + 1);
+                                   create_string_sixteenth lowest_string (main_start + 1);
+                                   create_string_eighth ~dot:true second_string (main_start + 1)]
 
-      let last = create_measure [create_string_sixteenth second_string 7;
-                                 create_string_eighth ~dot:true third_string 4;
-                                 create_string_sixteenth third_string 5;
-                                 create_string_quarter second_string 7;
-                                 create_string_quarter ~tied:(Some `Start) second_string 5;
-                                 create_string_eighth ~dot:true ~tied:(Some `Stop) second_string 5]
+      let second_bis = create_measure [create_string_sixteenth third_string (bis_start + 2);
+                                       create_string_eighth ~dot:true third_string bis_start;
+                                       create_string_sixteenth third_string (bis_start + 2);
+                                       create_string_eighth ~dot:true third_string bis_start;
+                                       create_string_sixteenth second_string (bis_start + 3);
+                                       create_string_eighth ~dot:true ~tied:(Some `Start) second_string (bis_start + 2);
+                                       create_string_sixteenth lowest_string (bis_start + 2);
+                                       create_string_eighth ~dot:true second_string (bis_start + 2)]
+
+      let third = create_measure [create_string_sixteenth second_string (main_start + 3);
+                                   create_string_eighth ~dot:true third_string main_start;
+                                   create_string_sixteenth third_string (main_start + 1);
+                                   create_string_quarter second_string (main_start + 3);
+                                   create_string_quarter ~tied:(Some `Start) second_string (main_start + 3);
+                                   create_string_eighth ~dot:true ~tied:(Some `Stop) second_string (main_start + 3)]
+
+      let third_bis = create_measure [create_string_sixteenth second_string (bis_start + 3);
+                                      create_string_eighth ~dot:true third_string bis_start;
+                                      create_string_sixteenth third_string (bis_start + 2);
+                                      create_string_quarter second_string (bis_start + 3);
+                                      create_string_quarter ~tied:(Some `Start) second_string (bis_start + 3);
+                                      create_string_eighth ~dot:true ~tied:(Some `Stop) second_string (bis_start + 3)]
+
+      let last = create_measure [create_string_sixteenth second_string (main_start + 3);
+                                 create_string_eighth ~dot:true third_string main_start;
+                                 create_string_sixteenth third_string (main_start + 1);
+                                 create_string_quarter second_string (main_start + 3);
+                                 create_string_quarter ~tied:(Some `Start) second_string (main_start + 1);
+                                 create_string_eighth ~dot:true ~tied:(Some `Stop) second_string (main_start + 1)]
+
+      let last_bis = create_measure [create_string_sixteenth second_string (bis_start + 3);
+                                     create_string_eighth ~dot:true third_string bis_start;
+                                     create_string_sixteenth third_string (bis_start + 2);
+                                     create_string_quarter second_string (bis_start + 3);
+                                     create_string_quarter ~tied:(Some `Start) second_string (bis_start + 2);
+                                     create_string_eighth ~dot:true ~tied:(Some `Stop) second_string (bis_start + 1)]
 
       let t = [first; second; third; first; second; last]
+      let t_bis = [first_bis; second_bis; third_bis; first_bis; second_bis; last_bis]
       end
+
     let guitar_measures = F.t @ S.t
-    let guitar_two_measures = transpose_measures 4 guitar_measures
-    let t = create_part guitar_measures guitar_two_measures
+    let guitar_bis_measures = F.t_bis @ S.t_bis
+    let t = create_part guitar_measures guitar_bis_measures
   end
 
 module Example = struct
