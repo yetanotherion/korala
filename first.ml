e552ae2ed947adfac6fa90af6c2c09fbb2a625c0
Ion Alberdi <nolaridebi@gmail.com>
2015-11-22 17:57:56 +0100

    first.ml: end of the song

--- first.ml
+++ first.ml
@@ -20,38 +20,75 @@ let flatten l =
                      append_parts accum x) hd tl
 
 module B = struct
-    let first = create_measure
-                  [create_string_quarter 3 4;
-                   create_string_eighth ~dot:true 3 4;
-                   create_string_sixteenth 3 5;
-                   create_string_eighth ~dot:true 3 4;
-                   create_string_sixteenth 2 7;
-                   create_string_eighth ~dot:true 2 5;
-                   create_string_sixteenth 2 5]
+    module F = struct
+      let first = create_measure
+                    [create_string_quarter 3 4;
+                     create_string_eighth ~dot:true 3 4;
+                     create_string_sixteenth 3 5;
+                     create_string_eighth ~dot:true 3 4;
+                     create_string_sixteenth 2 7;
+                     create_string_eighth ~dot:true 2 5;
+                     create_string_sixteenth 2 5]
 
-    let second = create_measure [create_string_eighth ~dot:true 2 7;
-                                 create_string_sixteenth 2 7;
-                                 create_string_eighth ~dot:true 2 5;
-                                 create_string_sixteenth 2 7;
-                                 create_string_quarter 3 4;
-                                 create_string_quarter 2 7]
-    let third = create_measure
-                  [create_string_quarter 3 4;
-                   create_string_eighth ~dot:true 3 7;
-                   create_string_sixteenth 3 5;
-                   create_string_eighth ~dot:true 3 4;
-                   create_string_sixteenth 2 7;
-                   create_string_eighth ~dot:true 2 5;
-                   create_string_sixteenth 1 5]
+      let second = create_measure [create_string_eighth ~dot:true 2 7;
+                                   create_string_sixteenth 2 7;
+                                   create_string_eighth ~dot:true 2 5;
+                                   create_string_sixteenth 2 7;
+                                   create_string_quarter 3 4;
+                                   create_string_quarter 2 7]
+      let third = create_measure
+                    [create_string_quarter 3 4;
+                     create_string_eighth ~dot:true 3 7;
+                     create_string_sixteenth 3 5;
+                     create_string_eighth ~dot:true 3 4;
+                     create_string_sixteenth 2 7;
+                     create_string_eighth ~dot:true 2 5;
+                     create_string_sixteenth 1 5]
 
-    let fourth = create_measure [create_string_eighth ~dot:true 2 5;
-                                 create_string_sixteenth 2 7;
+      let fourth = create_measure [create_string_eighth ~dot:true 2 5;
+                                   create_string_sixteenth 2 7;
+                                   create_string_eighth ~dot:true 3 4;
+                                   create_string_sixteenth 3 5;
+                                   create_string_quarter 2 7;
+                                   create_string_quarter 2 5]
+      let t = [first; second; third; fourth]
+      end
+    module S = struct
+      let first = create_measure
+                    [create_string_sixteenth 3 4;
+                     create_string_eighth ~dot:true 2 5;
+                     create_string_sixteenth 3 4;
+                     create_string_eighth ~dot:true 3 4;
+                     create_string_sixteenth 3 4;
+                     create_string_eighth ~dot:true ~tied:(Some `Start) 3 5;
+                     create_string_quarter ~tied:(Some `Stop) 3 5]
+
+      let second = create_measure [create_string_sixteenth 3 5;
+                                   create_string_eighth ~dot:true 3 4;
+                                   create_string_sixteenth 3 5;
+                                   create_string_eighth ~dot:true 3 4;
+                                   create_string_sixteenth 2 7;
+                                   create_string_eighth ~dot:true ~tied:(Some `Start) 2 5;
+                                   create_string_sixteenth 1 5;
+                                   create_string_eighth ~dot:true 2 5]
+
+      let third = create_measure [create_string_sixteenth 2 7;
+                                   create_string_eighth ~dot:true 3 4;
+                                   create_string_sixteenth 3 5;
+                                   create_string_quarter 2 7;
+                                   create_string_quarter ~tied:(Some `Start) 2 7;
+                                   create_string_eighth ~dot:true ~tied:(Some `Stop) 2 7]
+
+      let last = create_measure [create_string_sixteenth 2 7;
                                  create_string_eighth ~dot:true 3 4;
                                  create_string_sixteenth 3 5;
                                  create_string_quarter 2 7;
-                                 create_string_quarter 2 5]
+                                 create_string_quarter ~tied:(Some `Start) 2 5;
+                                 create_string_eighth ~dot:true ~tied:(Some `Stop) 2 5]
 
-    let t = create_part [first; second; third; fourth]
+      let t = [first; second; third; first; second; last]
+      end
+    let t = create_part (F.t @ S.t)
   end
 
 module Example = struct
